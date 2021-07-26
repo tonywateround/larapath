4392eef42e1228ea659546d0de74687ec809fe48
tonywateround <84058185+tonywateround@users.noreply.github.com>
2021-07-26 17:24:50 -0400

    Update install_larapath.sh

--- install_larapath.sh
+++ install_larapath.sh
@@ -3,5 +3,6 @@
 curl https://raw.githubusercontent.com/tonywateround/larapath/main/larapath --output larapath
 mv larapath /usr/local/bin
 chmod +x /usr/local/bin/larapath
+echo "" > ~/.larapath
 echo "Larapath has been installed successfully."
 echo "You can run $larapath -h or --help for list of available commands"
