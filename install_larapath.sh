417d1acf93ff0ca93cbcca1b8ef12ae1ff380785
tonywateround <84058185+tonywateround@users.noreply.github.com>
2021-07-27 02:48:40 -0400

    Update install_larapath.sh

--- install_larapath.sh
+++ install_larapath.sh
@@ -7,5 +7,5 @@ echo  "alias larapath='. /usr/local/bin/larapath_cli'" > ~/.larapath
 echo "alias larapath='. /usr/local/bin/larapath_cli'" >> ~/.bashrc
 echo "$(tput setaf 2)✓ $(tput setaf 6)Larapath $(tput setaf 2)has been installed successfully.$(tput sgr 0)"
 echo "$(tput setaf 5)Then run $(tput setaf 2)larapath -h$(tput setaf 5) for list of commands\n"
-source ~/.bashrc
+. ~/.bashrc
 rm install_larapath.sh
