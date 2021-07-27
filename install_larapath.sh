53166a4a88310efa4104c3fc8bb5be6900ee8ab4
tonywateround <84058185+tonywateround@users.noreply.github.com>
2021-07-27 01:55:17 -0400

    Update install_larapath.sh

--- install_larapath.sh
+++ install_larapath.sh
@@ -9,3 +9,4 @@ echo "$(tput setaf 5)Add this $(tput setaf 6)alias$(tput setaf 5) in your env to
 echo "$(tput setaf 6)alias larapath='. /usr/local/bin/larapath_cli'$(tput sgr 0)"
 echo "$(tput setaf 5)Then, source your env, eg: $(tput setaf 2)$ source ~/.bashrc $(tput setaf 5) or reload terminal$(tput sgr 0)" 
 echo "$(tput setaf 5)After sourcing or reload, run $(tput setaf 2)larapath -h$(tput setaf 5) for list of commands\n"
+rm install_larapath.sh
