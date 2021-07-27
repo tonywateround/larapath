0d29cd35d123738f41bce95faeb00dab430035fd
tonywateround <84058185+tonywateround@users.noreply.github.com>
2021-07-27 01:53:59 -0400

    Update install_larapath.sh

--- install_larapath.sh
+++ install_larapath.sh
@@ -5,7 +5,7 @@ mv larapath_cli /usr/local/bin
 chmod +x /usr/local/bin/larapath_cli
 echo  "alias larapath='. /usr/local/bin/larapath_cli'" > ~/.larapath
 echo "$(tput setaf 2)✓ $(tput setaf 6)Larapath $(tput setaf 2)has been installed successfully.$(tput sgr 0)"
-echo "$(tput setaf 1)Add this $(tput setaf 6)alias$(tput setaf 1) in your env to bypass CD restriction in bash script$(tput sgr 0)"
+echo "$(tput setaf 5)Add this $(tput setaf 6)alias$(tput setaf 5) in your env to bypass CD restriction in bash script$(tput sgr 0)"
 echo "$(tput setaf 6)alias larapath='. /usr/local/bin/larapath_cli'$(tput sgr 0)"
-echo "$(tput setaf 1)Then, source your env, eg: $(tput setaf 2)$ source ~/.bashrc $(tput setaf 1) or reload terminal$(tput sgr 0)" 
-echo "$(tput setaf 1) After sourcing or reload, run $(tput setaf 2)larapath -h$(tput setaf 1) for list of commands\n"
+echo "$(tput setaf 5)Then, source your env, eg: $(tput setaf 2)$ source ~/.bashrc $(tput setaf 5) or reload terminal$(tput sgr 0)" 
+echo "$(tput setaf 5)After sourcing or reload, run $(tput setaf 2)larapath -h$(tput setaf 5) for list of commands\n"
