b12a1175ab24ce01a8913f1b5dd2c6ae9d799b7e
tonywateround <84058185+tonywateround@users.noreply.github.com>
2021-07-27 01:45:15 -0400

    Update install_larapath.sh

--- install_larapath.sh
+++ install_larapath.sh
@@ -5,4 +5,8 @@ mv larapath_cli /usr/local/bin
 chmod +x /usr/local/bin/larapath_cli
 echo  "alias larapath='. /usr/local/bin/larapath_cli'" > ~/.larapath
 echo "$(tput setaf 2)✓ $(tput setaf 6)Larapath $(tput setaf 2)has been installed successfully.$(tput sgr 0)"
-echo "$(tput setaf 2) You can run $(tput setaf 6)larapath --help$(tput setaf 2) for list of available commands"
+echo "$(tput setaf 1)You add custom alias in your environment variable to bypass CD cmd restriction in bash script(tput sgr 0)"
+echo "$(tput setaf 6)alias larapath='. /usr/local/bin/larapath_cli'$(tput sgr 0)"
+echo "$(tput setaf 1) Then, $(tput setaf 6)$ source ~/.bashrc $(tput setaf 1) or reload terminal$(tput sgr 0)" 
+echo "$(tput setaf 1)(You are free to use the environment file of your choice)$(tput sgr 0)\n"
+echo "$(tput setaf 1) After sourcing, you can run $(tput setaf 6)larapath --help$(tput setaf 1) for list of available commands\n"
